182efa62cc86869663b0b019f8628b2a9ca4768c
daptanque <daniel.apeixoto.tanque@gmail.com>
2024-02-19 10:56:13 +0000

    Sequential Coroutines

--- app/src/main/java/com/example/coroutineseqapp/MainActivity.kt
+++ app/src/main/java/com/example/coroutineseqapp/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.coroutineseqapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val one = doSomeThingUseful1()
+            val two = doSomeThingUseful2()
+
+            val result = one + two
+            Log.v("TAGY", "The result is: $result ")
+        }
+    }
+
+    suspend fun doSomeThingUseful1() : Int{
+        delay(5000)
+        Log.v("TAGY", "Func1 is done")
+        return 11
+    }
+
+    suspend fun doSomeThingUseful2() : Int{
+        delay(7000)
+        Log.v("TAGY", "Func2 is done")
+        return 8
     }
 }
